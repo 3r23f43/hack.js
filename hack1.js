9604d774c069b801558360c90e1e133df0444440
3r23f43 <45143230+3r23f43@users.noreply.github.com>
2019-10-28 01:21:59 +0300

    Update hack1.js

--- hack1.js
+++ hack1.js
@@ -1,1 +1,13 @@
-alert(1);
+const PolitHack = (function() {
+	const COLOR = {
+		HAS_MISTAKES: 'feffa3',
+		CORRECT: 'a3c8ff',
+		SERIES: '9bf7bc',
+		WRONG: 'ffa3a3'
+	};
+
+	function bubble() {
+		alert("1");
+	}
+  bubble();
+
